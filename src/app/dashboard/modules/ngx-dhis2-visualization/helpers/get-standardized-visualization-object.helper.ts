59946f47727c4350395e0d67b94beeef11984d66
Rajab Mkomwa <rajeyw@gmail.com>
2019-03-09 23:48:39 +0300

    fix: remove id auto-generation for non favorite visualization

--- src/app/dashboard/modules/ngx-dhis2-visualization/helpers/get-standardized-visualization-object.helper.ts
+++ src/app/dashboard/modules/ngx-dhis2-visualization/helpers/get-standardized-visualization-object.helper.ts
@@ -3,8 +3,12 @@ import * as _ from 'lodash';
 import { generateUid } from '../../../../helpers/generate-uid.helper';
 import { checkIfVisualizationIsNonVisualizable } from './check-if-visualization-is-non-visualizable.helper';
 
-export function getStandardizedVisualizationObject(visualizationItem: any): Visualization {
-  const isNonVisualizable = checkIfVisualizationIsNonVisualizable(visualizationItem.type);
+export function getStandardizedVisualizationObject(
+  visualizationItem: any
+): Visualization {
+  const isNonVisualizable = checkIfVisualizationIsNonVisualizable(
+    visualizationItem.type
+  );
   const visualizationObject = {
     id: visualizationItem.id,
     title: getVisualizationTitle(visualizationItem),
@@ -22,9 +26,22 @@ export function getStandardizedVisualizationObject(visualizationItem: any): Visu
       percent: 0,
       message: 'Loading..'
     },
-    layers: []
+    layers: getVisualizationLayers(visualizationItem)
   };
   return visualizationObject;
+}
+
+function getVisualizationLayers(visualizationItem: any) {
+  if (!visualizationItem) {
+    return [];
+  }
+
+  const favoriteItemArray =
+    visualizationItem[_.camelCase(visualizationItem.type)];
+
+  return _.isArray(favoriteItemArray)
+    ? (favoriteItemArray || []).map((item: any) => item.id)
+    : [];
 }
 
 function getVisualizationName(visualizationItem: any) {
@@ -46,22 +63,26 @@ function getVisualizationName(visualizationItem: any) {
     default:
       return visualizationItem.name
         ? visualizationItem.name
-        : visualizationItem.type && visualizationItem.hasOwnProperty(_.camelCase(visualizationItem.type))
-          ? _.isPlainObject(visualizationItem[_.camelCase(visualizationItem.type)])
-            ? visualizationItem[_.camelCase(visualizationItem.type)].displayName
-            : 'Untitled'
-          : 'Untitled';
+        : visualizationItem.type &&
+          visualizationItem.hasOwnProperty(_.camelCase(visualizationItem.type))
+        ? _.isPlainObject(
+            visualizationItem[_.camelCase(visualizationItem.type)]
+          )
+          ? visualizationItem[_.camelCase(visualizationItem.type)].displayName
+          : 'Untitled'
+        : 'Untitled';
   }
 }
 
 function getVisualizationTitle(visualizationItem: any) {
   return visualizationItem.title
     ? visualizationItem.title
-    : visualizationItem.type && visualizationItem.hasOwnProperty(_.camelCase(visualizationItem.type))
-      ? _.isPlainObject(visualizationItem[_.camelCase(visualizationItem.type)])
-        ? visualizationItem[_.camelCase(visualizationItem.type)].title
-        : undefined
-      : undefined;
+    : visualizationItem.type &&
+      visualizationItem.hasOwnProperty(_.camelCase(visualizationItem.type))
+    ? _.isPlainObject(visualizationItem[_.camelCase(visualizationItem.type)])
+      ? visualizationItem[_.camelCase(visualizationItem.type)].title
+      : undefined
+    : undefined;
 }
 
 function getFavoriteDetails(visualizationItem: any) {
@@ -77,9 +98,5 @@ function getFavoriteDetails(visualizationItem: any) {
         useTypeAsBase: true,
         requireAnalytics: true
       }
-    : {
-        id: generateUid(),
-        name: getVisualizationName(visualizationItem),
-        type: _.camelCase(visualizationItem.type)
-      };
+    : null;
 }
