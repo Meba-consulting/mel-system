9a170937173aab7d942065f21b2d58798d2be4a3
Rajab Mkomwa <rajeyw@gmail.com>
2019-03-27 14:23:39 +0300

    style: correct style issues on map loading spinner

--- src/app/dashboard/modules/ngx-dhis2-visualization/modules/map/components/map-loader/map-loader.component.ts
+++ src/app/dashboard/modules/ngx-dhis2-visualization/modules/map/components/map-loader/map-loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-map-loader',
@@ -14,6 +14,8 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class MapLoaderComponent implements OnInit {
+  @Input()
+  height: string;
   constructor() {}
 
   ngOnInit() {}
