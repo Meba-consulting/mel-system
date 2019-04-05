ac2748cac6e64fe1ea105ffa6f957ce1827ba1f2
Rajab Mkomwa <rajeyw@gmail.com>
2019-04-05 12:11:42 +0300

    feat: set application title using manifest name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
 
 import { State, LoadSystemInfo } from './store';
+import { Title } from '@angular/platform-browser';
+import { ManifestService, Manifest } from '@hisptz/ngx-dhis2-http-client';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private store: Store<State>,
-    private translate: TranslateService
-  ) {
+    private translate: TranslateService,
+    private titleService: Title,
+    private manifestService: ManifestService
+  ) {}
+
+  ngOnInit() {
     // Load system information
-    store.dispatch(new LoadSystemInfo());
+    this.store.dispatch(new LoadSystemInfo());
 
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
+    this.translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    this.translate.use('en');
+
+    // set title after loading it from manifest file
+    this.manifestService
+      .getManifest()
+      .pipe(catchError(() => of(null)))
+      .subscribe((manifest: Manifest) => {
+        if (manifest) {
+          this.titleService.setTitle(manifest.name);
+        } else {
+          this.titleService.setTitle('Interactive Dashboard');
+        }
+      });
   }
 }
