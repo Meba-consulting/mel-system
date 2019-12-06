40ce02ae65edf348486728a66811b131b077a06d
Rajab Mkomwa <rajeyw@gmail.com>
2019-12-07 02:04:27 +0300

    fix: resolve excel download issues

--- src/app/dashboard/modules/ngx-dhis2-visualization/services/visualization-export.service.ts
+++ src/app/dashboard/modules/ngx-dhis2-visualization/services/visualization-export.service.ts
@@ -8,46 +8,9 @@ export class VisualizationExportService {
     if (this._getMsieVersion() || this._isFirefox()) {
       console.error('Not supported browser');
     }
-    // Other Browser can download xls
-    // if (htmlTable) {
-    //   const uri = 'data:application/vnd.ms-excel;base64,',
-    //     template =
-    //       '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:' +
-    //       'office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook>' +
-    //       '<x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>' +
-    //       '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->' +
-    //       '</head><body><table border="1">{table}</table><br /><table border="1">{table}</table></body></html>',
-    //     base64 = s => window.btoa(unescape(encodeURIComponent(s))),
-    //     format = (s, c) => s.replace(/{(\w+)}/g, (m, p) => c[p]);
-
-    //   const ctx = { worksheet: 'Sheet 1', filename: filename };
-    //   let str =
-    //     '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office' +
-    //     ':excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook>' +
-    //     '<x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>' +
-    //     '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body>';
-
-    //   const matchedTableContent = htmlTable.match(
-    //     /<table[^>]*>([\w|\W]*)<\/table>/im
-    //   );
-    //   ctx['table1'] =
-    //     matchedTableContent && matchedTableContent.length > 1
-    //       ? matchedTableContent[1]
-    //       : '';
-
-    //   str +=
-    //     '<b>{filename}</b><br/><table border="1">{table1}</table></body></html>';
-
-    //   setTimeout(() => {
-    //     const link = document.createElement('a');
-    //     link.download = filename + '.xls';
-    //     link.href = uri + base64(format(str, ctx));
-    //     link.click();
-    //   }, 100);
-    // }
 
     {
-      const html = document.getElementById(tableId).innerHTML;
+      const html = document.getElementById(tableId).outerHTML;
 
       const filename = `${fileName}.xls`;
 
@@ -60,7 +23,7 @@ export class VisualizationExportService {
         '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office' +
         ':excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook>' +
         '<x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>' +
-        '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body>';
+        '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><style>td, th {border: solid thin;}</style></head><body>';
       str += html + '</body></html>';
       setTimeout(() => {
         const link = document.createElement('a');
