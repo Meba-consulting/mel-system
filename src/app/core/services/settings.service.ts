eb88fdc32210ef59413bbe3755a1f6f9e5233e98
josephatj <josephatjulius24@gmail.com>
2020-12-07 17:51:39 +0300

    add settings module

---
+++ src/app/core/services/settings.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { NgxDhis2HttpClientService } from '@iapps/ngx-dhis2-http-client';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SettingsService {
+  constructor(private httpClient: NgxDhis2HttpClientService) {}
+
+  getAttributes(): Observable<any> {
+    return this.httpClient.get(
+      'attributes.json?fields=id,name,optionSet[id,name,options[id,name]],organisationUnitAttribute&paging=false'
+    );
+  }
+}
