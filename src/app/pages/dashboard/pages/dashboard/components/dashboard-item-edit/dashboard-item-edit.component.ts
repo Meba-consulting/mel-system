af4d04d3bde030feee28550f28e11c2c280b82d2
josephatj <josephatjulius24@gmail.com>
2021-04-18 19:19:03 +0300

    fix dashboard editing bug

--- src/app/pages/dashboard/pages/dashboard/components/dashboard-item-edit/dashboard-item-edit.component.ts
+++ src/app/pages/dashboard/pages/dashboard/components/dashboard-item-edit/dashboard-item-edit.component.ts
@@ -7,7 +7,7 @@ import { catchError, map } from 'rxjs/operators';
 @Component({
   selector: 'app-dashboard-item-edit',
   templateUrl: './dashboard-item-edit.component.html',
-  styleUrls: ['./dashboard-item-edit.component.css']
+  styleUrls: ['./dashboard-item-edit.component.css'],
 })
 export class DashboardItemEditComponent implements OnInit {
   dashboard: any;
@@ -16,41 +16,49 @@ export class DashboardItemEditComponent implements OnInit {
   updateResponse$: Observable<any>;
   saving: boolean = false;
   changed: boolean = false;
-  constructor(private dialogRef: MatDialogRef<DashboardItemEditComponent>, @Inject(MAT_DIALOG_DATA) data, private httpClient: NgxDhis2HttpClientService) {
+  constructor(
+    private dialogRef: MatDialogRef<DashboardItemEditComponent>,
+    @Inject(MAT_DIALOG_DATA) data,
+    private httpClient: NgxDhis2HttpClientService
+  ) {
     this.dashboard = data?.dashboard;
     this.dashboardName = this.dashboard?.name;
     this.description = this.dashboard?.description;
-   }
-
-  ngOnInit(): void {
   }
+
+  ngOnInit(): void {}
 
   onSave(e) {
     this.saving = true;
     this.changed = false;
     const data = {
-      "description": this.description,
-      "id": this.dashboard?.id,
-      "name": this.dashboardName,
-      "favorites": this.dashboard?.favorite,
-      "userGroupAccesses": this.dashboard?.userGroupAccesses,
-      "dashboardItems": this.dashboard?.dashboardItems,
-      "userAccesses": this.dashboard?.userAccesses
-    }
+      description: this.description,
+      id: this.dashboard?.id,
+      name: this.dashboardName,
+      favorites: this.dashboard?.favorite,
+      userGroupAccesses: this.dashboard?.userGroupAccesses,
+      dashboardItems: this.dashboard?.dashboardItems,
+      userAccesses: this.dashboard?.userAccesses,
+    };
     e.stopPropagation();
-    this.updateResponse$ = this.httpClient.put('dashboards/' + this.dashboard?.id+'?mergeMode=UPDATE', data).pipe(map(response => {
-      this.saving = false;
-      this.changed = true;
-      return response
-    }),catchError(e => {
-      this.saving = false;
-      return of(e)
-    }))
+    console.log('test', data);
+    this.updateResponse$ = this.httpClient
+      .put('dashboards/' + this.dashboard?.id + '?mergeMode=UPDATE', data)
+      .pipe(
+        map((response) => {
+          this.saving = false;
+          this.changed = true;
+          return response;
+        }),
+        catchError((e) => {
+          this.saving = false;
+          return of(e);
+        })
+      );
   }
 
   onClose(e) {
-    e.stopPropagation()
-    this.dialogRef.close(this.changed)
+    e.stopPropagation();
+    this.dialogRef.close(this.changed);
   }
-
 }
