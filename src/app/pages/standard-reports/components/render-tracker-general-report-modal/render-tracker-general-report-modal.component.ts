7c85e70630b778f6ce788e5ed48a17b35eb1ccf4
josephatj <josephatjulius24@gmail.com>
2021-10-20 10:26:35 +0300

    add support to download to word doc

--- src/app/pages/standard-reports/components/render-tracker-general-report-modal/render-tracker-general-report-modal.component.ts
+++ src/app/pages/standard-reports/components/render-tracker-general-report-modal/render-tracker-general-report-modal.component.ts
@@ -36,14 +36,46 @@ export class RenderTrackerGeneralReportModalComponent implements OnInit {
     };
   }
 
-  ngOnInit(): void {
-    console.log(this.formattedTrackedEntityInstanceData);
-  }
+  ngOnInit(): void {}
 
   printPDF(event: Event) {
     event.stopPropagation();
     setTimeout(function () {
       window.print();
     }, 500);
+  }
+
+  public download(id, filename): void {
+    var preHtml =
+      "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+    var postHtml = '</body></html>';
+    var html = preHtml + document.getElementById(id).innerHTML + postHtml;
+
+    var blob = new Blob(['\ufeff', html], {
+      type: 'application/msword',
+    });
+
+    // Specify link url
+    var url =
+      'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+
+    // Specify file name
+    filename = filename ? filename + '.doc' : 'document.doc';
+
+    // Create download link element
+    var downloadLink = document.createElement('a');
+
+    document.body.appendChild(downloadLink);
+
+    // Create a link to the file
+    downloadLink.href = url;
+
+    // Setting the file name
+    downloadLink.download = filename;
+
+    //triggering the function
+    downloadLink.click();
+
+    document.body.removeChild(downloadLink);
   }
 }
